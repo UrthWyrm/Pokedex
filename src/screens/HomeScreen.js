32a94e65edb3b67e884304cd12865e7bac39824c
Carl D. Bishop <48108505+UrthWyrm@users.noreply.github.com>
2021-01-08 22:34:41 -0800

    no message

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -33,7 +33,7 @@ const HomeScreen = props => {
             title="Current Card"
             results={filterResults('Pokémon')}
              />
-            <Text style={styles.textStyle}>You Can Do EEET!!</Text>
+            <Text style={styles.textStyle}>You Can Do EEEET!!</Text>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>{results.length}</Text>
         </View>
