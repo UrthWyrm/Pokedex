2e4812ffbe65519eeebcb747bbbc8ddec45dce9b
Carl D. Bishop <48108505+UrthWyrm@users.noreply.github.com>
2021-01-10 19:39:13 -0800

    no message

--- src/screens/CardStats.js
+++ src/screens/CardStats.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Poke from '../api/Poke';
 
 const CardStats = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
-    console.log(id);
+
+    console.log(result);
 
     const getResult = async (id) => {
         const response = await Poke.get(`/${id}`);
-        response.data;
+        setResult(response.data);
     };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
 
     return(
         <View style={{
